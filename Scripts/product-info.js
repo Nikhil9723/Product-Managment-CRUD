28a975ed886463b6fdd3fcbb4fdad386903e099c
Nikhil Prajapati <nikhil.prajapati@simformsolutions.com>
2025-04-10 12:06:04 +0530

    modify (prouct-info.js) add find in place of map for better experince convert all let to const which are not going to change

--- Scripts/product-info.js
+++ Scripts/product-info.js
@@ -1,34 +1,21 @@
-let productName = document.getElementById("product-name");
-console.log(productName);
+import storageHandller from "./storage-handllers.js";
 
-let productPrice = document.getElementById("product-price");
-let productDescription = document.getElementById("product-description")
-
-
-let ProductDetails = JSON.parse( localStorage.getItem("product") )|| []
-let productImage = document.getElementById("product-img")
-// console.log(ProductDetails);
-
-// Using a custom URL string
-// const myUrl1 = new URL(`http://127.0.0.1:5500/product.html?productId=`);
-
+const productName = document.getElementById("product-name");
+const productPrice = document.getElementById("product-price");
+const productDescription = document.getElementById("product-description")
+const ProductDetails = storageHandller.getStorage("product");
+const productImage = document.getElementById("product-img");
 
 // Using the current page's URL
 const myUrl2 = new URL(window.location.toLocaleString());
 
 const productId = myUrl2.searchParams.get("productId");
-console.log(productId);
 
-console.log(productImage.src);
+const productData = ProductDetails.find((item) => item.id === productId)
 
-
-ProductDetails.map((item) => {
-    console.log(item);
-    
-    if(item.id === productId) {
-        productName.textContent = item.name;
-        productPrice.textContent = `₹ ${item.price}`;
-        productDescription.textContent = item.description;
-        productImage.src = item.imgUrl;
-    }
-})
+if(productData) {
+productName.textContent =productData.name;
+productPrice.textContent = `₹ ${productData.price ?? defaultPrice}`;
+productDescription.textContent = productData.description;
+productImage.src = productData.imgUrl;
+}
